b68247524f47d1a3a32dea7da40306de14edea57
Kobe Jan Dave Hufancia <Kobehufancia@gmail.com>
2025-08-26 19:21:42 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+// JavaScript Fundamentals - Part 1
+// We'll write our code here!
+
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+console.log("js:", js);
+
+console.log("=== VARIABLES ===");
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let age = 30;
+console.log(age);
+age  = 50;
+console.log(age);
+
+const birthYear = 1991;
+console.log(birthYear);
+
+const PI = 3.1415;
+console.log(PI);
+
+var job = 'programmer';
+job = 'teacher';
+console.log(job);
+
+const country = "Philippines";
+const language = "Filipino";
+
+age = 23;
+age = 24;
+
+//good variable names
+// let firstName
+// let mycurrentJob 
+// const PI
+
+// bad variable names
+// let 3years
+// let jonas&matilda
+// let now
+
+console.log("=== DATA TYPES ===");
+
+// Number
+let id = 12345;
+console.log(id);
+console.log(typeof id);
+
+// String
+let lastname ="Doe";
+console.log(lastname);
+console.log(typeof lastname);   
+
+// Boolean
+let JavaScriptisFun = true;
+console.log(JavaScriptisFun);
+console.log(typeof JavaScriptisFun);
+
+// Undefined
+let year;
+console.log(year);
+console.log(typeof year);  
+
+let dynamicVariable = 40;
+console.log(dynamicVariable);
+console.log(typeof dynamicVariable); 
+
+// change to a string
+dynamicVariable = "I am now a string";
+console.log(dynamicVariable);
+console.log(typeof dynamicVariable);
+
+// change to a boolean
+dynamicVariable = true;
+console.log(dynamicVariable);
+console.log(typeof dynamicVariable);
+
+
+
+
